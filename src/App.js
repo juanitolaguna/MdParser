73b906e11b6a8dec42eaf006717eefacd72258ea
juanitolaguna <little.pilgrim@mail.ru>
2019-03-19 03:12:18 +0100

    implement basic markdown to html functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'materialize-css'; // It installs the JS asset only
+import 'materialize-css/dist/css/materialize.min.css';
+
+import React, { Component } from 'react';
+import InputField from './components/InputField';
+import OutputField from './components/OutputField';
+
+//import data
+import list from './files/data.js';
+
+let md = require('markdown-it')();
+// let handlebars = require('handlebars');
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputTerm: list,
+    }
+  }
+
+  onDismiss = (id) => {
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
+  onInputChange = (event) => {
+    this.setState({ inputTerm: md.render(event.target.value) });
+  }
+
+  dummyMethod = (event) => {
+    return;
+  }
+
+
   render() {
+
+    const { author, title, mdExample } = this.state.inputTerm;
+
+    const authorStr = Object.keys({ author })[0];
+    const titleStr = Object.keys({ title })[0];
+    const mdExampleStr = Object.keys({ mdExample })[0];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <br />
+        <div className="row">
+          <div className="col s6 input">
+            <form className="col s12">
+
+              <div className="row">
+                <div className="col s6">
+                  <InputField
+                    fieldName={authorStr}
+                    value={author}
+                    action={this.dummyMethod}
+                  />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col s6">
+                  <InputField
+                    fieldName={titleStr}
+                    value={title}
+                    action={this.dummyMethod}
+                  />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col s12">
+                  <InputField
+                    fieldName={mdExampleStr}
+                    value={mdExample}
+                    action={this.onInputChange}
+                  />
+                </div>
+              </div>
+
+            </form>
+          </div>
+
+          <div className="col s6 output">
+            <OutputField output={this.state.inputTerm}/>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
---
+++ src/files/data.js
@@ -0,0 +1,21 @@
+const list = {
+    author: 'Author',
+    title: 'Title',
+    mdExample: `
+# Nervoque per claudit inclita tendentemque surrexere tempora
+
+## Aemulus facinus
+
+[Lorem](http://aeraetiamnum.org/) markdownum aptos: [pedes
+alas](http://pressosmolimine.com/) sive Cromyona ore, obscurum, quique fama.
+Acumine velis dant est in obstipuit hominemque nullisque in fuit. Caelo in
+dumque ultor. Urbe saevis litora aequor reperitur creditis sacra, vaga? Agre si
+sequentia pocula suisque quam, aut intus silvas Iovemque.
+
+- Incerti sed surrexere Cycnus tamen dictis
+- Quam aequore corpora bracchia corpore
+- Nocte cogitat
+        `,
+}
+
+export default list;
